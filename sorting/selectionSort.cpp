5c854fc39273b94fc93eabbcc121eaf4a457e81b
Swastik Mukherjee <swastik.reach@gmail.com>
2021-07-12 09:30:01 +0545

    Selection sort program created using static array

---
+++ sorting/selectionSort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, tmp = 0;
+    cout << "Enter the number of elements : " << endl;
+
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter the elements : " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
